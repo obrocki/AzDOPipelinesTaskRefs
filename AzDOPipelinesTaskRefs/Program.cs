20b93718f5788fe1911a0c36630a5f486ba26a37
Dawid <83449241+obrocki@users.noreply.github.com>
2022-10-17 11:38:56 +0100

    initial check-in with code

---
+++ AzDOPipelinesTaskRefs/Program.cs
@@ -0,0 +1,226 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+FileStream? ostrm = null;
+StreamWriter writer;
+
+string? rootDir = Environment.CurrentDirectory;
+bool useFileOutput = false;
+
+if (args.Length >= 1)
+{
+    rootDir = args[0]?.ToString();
+
+    bool.TryParse(args[1], out useFileOutput);
+}
+
+if (useFileOutput)
+{
+    string outputFile = "output.txt";
+    TextWriter oldOut = Console.Out;
+    File.Delete(outputFile);
+    ostrm = new FileStream(outputFile, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    writer = new StreamWriter(ostrm);
+    Console.SetOut(writer);
+}
+
+Console.ForegroundColor = ConsoleColor.Gray;
+var taskList = await ParseTaskList();
+
+Console.WriteLine();
+
+var ymlFilePaths = FindYamlFiles(rootDir);
+
+Console.WriteLine();
+
+var markers = await ValidateFiles(taskList, ymlFilePaths);
+
+Console.WriteLine();
+
+WriteLineMarkers(markers);
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Gray;
+
+if (useFileOutput)
+{
+    await ostrm!.FlushAsync();
+    ostrm!.Close();
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Press any key to exit");
+    Console.ReadKey();
+}
+
+
+async Task<IEnumerable<string>> ParseTaskList()
+{
+    var outputTasks = new List<string>();
+
+    Console.WriteLine("Parsing Azure Pipelines Task Reference");
+
+    var rawHtmlResponse = await new HttpClient().GetStreamAsync("https://learn.microsoft.com/en-us/azure/devops/pipelines/tasks/reference/?view=azure-pipelines");
+
+    StreamReader streamReader = new StreamReader(rawHtmlResponse);
+
+    string? line;
+
+    while ((line = streamReader.ReadLine()) != null)
+    {
+        var firstWords = line.Replace("\t", " ").Replace("data-linktype=\"relative-path\">", "").Split(' ');
+
+        foreach (var firstWord in firstWords)
+        {
+            if (firstWord != null && firstWord.Contains("@") && firstWord.Contains("</a>"))
+                outputTasks.Add(firstWord.Remove(firstWord.IndexOf("</a>")));
+        }
+    }
+
+    // Extend the list of valida tasks
+    outputTasks.AddRange(new[]
+    {
+        "UseGitVersion@5",
+        "GitVersion@5",
+        "AzureRMWebAppDeployment@4",
+        "gitversion/setup@0",
+        "gitversion/execute@0",
+        "gittools.gitversion.gitversion-task.GitVersion@3",
+        "dependabot@1",
+        "PackageAzureDevOpsExtension@3",
+        "PublishAzureDevOpsExtension@3",
+        "DotNetCoreInstaller@2",
+        "TfxInstaller@3",
+        "IsAzureDevOpsExtensionValid@3",
+        "PublishPipelineArtifact@1",
+        "GitHubRelease@1",
+        "SnykSecurityScan@0"        
+    });
+
+    var parsedTasks = outputTasks.OrderBy(task => task).Distinct(new TaskComparer());
+
+    foreach (var task in parsedTasks)
+    {
+        Console.WriteLine($" - {task}");
+    }
+
+    Console.WriteLine($"{parsedTasks.Count()} tasks parsed.");
+
+    return parsedTasks;
+}
+
+List<FileInfo> FindYamlFiles(string startFolder)
+{
+    Console.WriteLine($"Looking up YML files in {startFolder} root directory.");
+
+    DirectoryInfo dir = new System.IO.DirectoryInfo(startFolder);
+
+    IEnumerable<FileInfo> fileList = dir.GetFiles("*.*", SearchOption.AllDirectories);
+
+    IEnumerable<FileInfo> fileQuery = from file in fileList
+                                      where file.Extension == ".yml" || file.Extension == ".yaml"
+                                      orderby file.Name
+                                      select file;
+
+    foreach (FileInfo fi in fileQuery)
+    {
+        Console.WriteLine($" - {fi.FullName}");
+    }
+
+    Console.WriteLine($"{fileQuery.Count()} files found.");
+
+    return fileQuery.ToList();
+}
+
+async Task<IEnumerable<CheckNeededMarker>> ValidateFiles(IEnumerable<string> taskList, IEnumerable<FileInfo> ymlFilePaths)
+{
+    int taskCount = 0;
+
+    List<CheckNeededMarker> checkNeededMarkers = new List<CheckNeededMarker>();
+
+    foreach (var ymlFile in ymlFilePaths)
+    {
+        var marker = new CheckNeededMarker(ymlFile);
+
+        IEnumerable<string> linesWithTasks = (await File.ReadAllLinesAsync(ymlFile.FullName)).Where(line => line.Contains("- task: "));
+
+        foreach (var line in linesWithTasks)
+        {
+            if (line.Split(new[] { ' ' }).Any(taskList.Contains))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine($"\tFile: {ymlFile.Name}\t{line}");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"\tFile: {ymlFile.Name}\t{line}");
+
+                var tasksNoVersion = taskList.Select(t => t.Remove(t.IndexOf("@")));
+                var invalidask = line.Remove(line.IndexOf("@")).Replace("- task: ", "");
+
+                if (tasksNoVersion.Any(x => x.ToLowerInvariant().Equals(invalidask.ToLowerInvariant())))
+                {
+                    Console.WriteLine($"Match found for {invalidask}");
+                }
+
+                marker.Lines.Add(line);
+                if (checkNeededMarkers.All(x => x.File.FullName != marker.File.FullName))
+                {
+                    checkNeededMarkers.Add(marker);
+                }
+            }
+        }
+
+        taskCount += linesWithTasks.Count();
+    }
+
+    Console.WriteLine($"{taskCount} tasks checked.");
+
+    return checkNeededMarkers;
+}
+
+void WriteLineMarkers(IEnumerable<CheckNeededMarker> markers)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine();
+    Console.WriteLine($"YML files requiring attention ({markers.Count()})");
+
+    foreach (var marker in markers)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine($"\tFile: {marker.File.Name} ({marker.Lines.Count()} task(s))");
+        foreach (var line in marker.Lines.Distinct())
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\t     {line.TrimStart()}");
+        }
+    }
+}
+
+class CheckNeededMarker
+{
+    public CheckNeededMarker(FileInfo ymlFile)
+    {
+        File = ymlFile;
+    }
+
+    public FileInfo File { get; set; }
+    public string ValidTask { get; set; }
+    public List<string> Lines { get; set; } = new List<string>();
+}
+
+class TaskComparer : EqualityComparer<string>
+{
+    public override bool Equals(string? x, string? y)
+    {
+        return x.ToLowerInvariant().Equals(y.ToLowerInvariant());
+    }
+
+    public override int GetHashCode([DisallowNull] string obj)
+    {
+        return obj.GetHashCode();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # AzDOPipelinesTaskRefs
-Scan YAML files for Azure DevOps Pipelines task references
+Scan YAML files for Azure DevOps Pipelines task references.
+
+Based on the scraped information off the [Azure Pipelines task reference article](https://learn.microsoft.com/azure/devops/pipelines/tasks/reference/?view=azure-pipelines), this rudimentary console app with check for any YML task definitions.
+
+## Usage
+
+```dotnetcli
+ .\AzDOYmlTasks.exe C:\Code\Pipelines false
+```
+
+Expected parameters:
+
+- [`String`] **Root directory of YML files**.
+
+  Subdirectories will be included in the search. E.g. C:\Code\Pipelines. Defaults to current execution directory.
+
+- [`Boolean`] **Console/File output toggle**.
+
+    When parameter is `true` the scan result will be printed out to `output.txt` file. Defaults to `Console` output (false).
+
+
+The reference list of tasks can be extended by adding name(s) of the task to `outputTasks` list (with addition of any known tasks (line:80 Program.cs).
+
+## Sample Output
+
+```dotnetcli
+> .\AzDOYmlTasks.exe C:\Code\Pipelines false
+Parsing Azure Pipelines Task Reference
+ - AndroidBuild@1
+ - AndroidSigning@1
+
+ // full list skipped
+
+ - Xcode@2
+ - Xcode@3
+ - Xcode@4
+ - Xcode@5
+ - XcodePackageiOS@0
+246 tasks parsed.
+
+Looking up YML files in C:\Code\Pipelines root directory.
+ - C:\Code\Pipelines\azure-pipelines-AZSK.yml
+ - C:\Code\Pipelines\azure-pipelines-CredScan.yml
+ - C:\Code\Pipelines\azure-pipelines-SAST.yml
+ - C:\Code\Pipelines\azure-pipelines-SCA.yml
+ - C:\Code\Pipelines\azure-pipelines.yml
+ - C:\Code\Pipelines\docker-compose.ci.build.yml
+ - C:\Code\Pipelines\docker-compose.override.yml
+ - C:\Code\Pipelines\docker-compose.yml
+ - C:\Code\Pipelines\sauce_browsers.yml
+9 files found.
+
+        File: azure-pipelines-AZSK.yml      - task: AzSKSVTs@4
+        File: azure-pipelines-CredScan.yml          - task: UsePythonVersion@0
+        File: azure-pipelines-CredScan.yml          - task: CmdLine@2
+        File: azure-pipelines-CredScan.yml          # - task: CmdLine@2
+        File: azure-pipelines-CredScan.yml          - task: PublishPipelineArtifact@1
+        File: azure-pipelines-CredScan.yml          - task: PowerShell@2
+        File: azure-pipelines-SAST.yml      - task: SonarQubePrepare@4
+        File: azure-pipelines-SAST.yml      - task: SonarQubeAnalyze@4
+        File: azure-pipelines-SAST.yml      - task: SonarQubePublish@4
+        File: azure-pipelines-SCA.yml       - task: WhiteSource Bolt@20
+        File: azure-pipelines.yml           - task: DockerCompose@0
+        File: azure-pipelines.yml           - task: DockerCompose@0
+        File: azure-pipelines.yml           - task: DockerCompose@0
+        File: azure-pipelines.yml           - task: DockerCompose@0
+        File: azure-pipelines.yml                 - task: SqlAzureDacpacDeployment@1
+        File: azure-pipelines.yml                 - task: AzureWebAppContainer@1
+        File: azure-pipelines.yml       #           - task: SqlAzureDacpacDeployment@1
+        File: azure-pipelines.yml       #           - task: AzureWebAppContainer@1
+18 tasks checked.
+
+
+YML files requiring attention (2)
+        File: azure-pipelines-AZSK.yml (1 task(s))
+             - task: AzSKSVTs@4
+        File: azure-pipelines-SCA.yml (1 task(s))
+             - task: WhiteSource Bolt@20
+
+
+Press any key to exit
+```
